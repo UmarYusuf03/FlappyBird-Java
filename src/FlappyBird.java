4c6007b7bb6257b70b77c072c357ce30730c4767
UmarYusuf03 <umaryusuf0307@gmail.com>
2026-09-13 17:17:18 +0530

    app.java done

---
+++ src/FlappyBird.java
@@ -0,0 +1,27 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Random;
+import javax.swing.*;
+
+
+public class FlappyBird extends JPanel{
+    int boardWidth = 360;
+    int boardHeight = 640;
+
+    //Images
+
+    Image backgroundImg;
+    Image birdImg;
+    Image topPipeImg;
+    Image bottomPipeImg;
+
+    FlappyBird(){
+        setPreferredSize(new Dimension(boardWidth, boardHeight));
+        setBackground(Color.blue);
+
+        //load Images
+        backgroundImg = new ImageIcon(getClass().getResource("./flappybirdbg.png")).getImage();
+        
+    }
+}
